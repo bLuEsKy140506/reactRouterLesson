bf311b9ffe09a247d01482e88cfb3be5a6707e7d
bLuEsKy140506 <115715458+bLuEsKy140506@users.noreply.github.com>
2025-06-01 22:23:12 +0800

    2nd commit, configure the primary paths

--- src/app/App.js
+++ src/app/App.js
@@ -10,21 +10,35 @@ import Profile from "../components/Profile";
 import EditProfileForm from "../components/EditProfileForm";
 import Root from "../components/Root";
 
-import { Route } from 'react-router-dom';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 import "./App.css";
 
-const router = 
-  /* Wrap this Root Route to create Router here */
-  <Route path="/" element={ <Root/> }>
-    {/* Add Routes here! */}
-  </Route>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="about" element={<About />} />
+      <Route path="sign-up" element={<SignUp />} />
+      <Route path="articles" element={<Articles />} />
+      {/* <Route path="articles/:id" element={<Article />} /> */}
+      <Route path="categories" element={<Categories />} />
+      {/* <Route path="categories/:id" element={<Category />} /> */}
+      <Route path="authors/:id" element={<Author />} />
+      <Route path="profile" element={<Profile />} />
+      {/* Add Routes here! */}
+    </Route>
+  )
+);
 
 function App() {
   return (
     <>
-      { /* Replace below and add Router Provider*/}
-      <p>REPLACE ME WITH A ROUTER PROVIDER</p>
+      <RouterProvider router={router} />
     </>
   );
 }
